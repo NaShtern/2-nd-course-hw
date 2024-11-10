9ae737c1291e46bc68a7274975606ae4ea3afc54
NaShtern <nagaychik15@gmail.com>
2024-11-10 18:49:54 +0300

    Задание 7

--- hw-2.js
+++ hw-2.js
@@ -48,7 +48,18 @@ let result = 2 ** 5;
 alert(result);
 */
 
+/* Задание 6
 let a = 9;
 let b = 2;
 let remainder = a % b;
 alert(remainder);
+*/
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
