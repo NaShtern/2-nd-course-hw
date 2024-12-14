a4474b5463013d75427e3e2977628b0da6023f0c
NaShtern <nagaychik15@gmail.com>
2024-12-14 15:45:42 +0300

    Исправления

--- hw-2.js
+++ hw-2.js
@@ -159,19 +159,14 @@ switch (monthNumber) {
 /* Урок 4*/
 
 /* Задание 1
-let i = 0;
-while (i < 2) {
+for (let i = 0; i < 2; i++) {
   console.log("Привет");
-  i++;
 }
-  */
+*/
 
 /* Задание 2
-let i = 1;
-
-while (i <= 5) {
+for (let i = 1; i <= 5; i++) {
   console.log(i);
-  i++;
 }
   */
 
@@ -183,15 +178,15 @@ for (let i = 7; i <= 22; i++) {
 
 /* Задание 4
 const obj = {
-  n: "200",
-  v: "300",
-  p: "400",
+  Коля: "200",
+  Вася: "300",
+  Петя: "400",
 };
 
 for (const key in obj) {
-  console.log(`${key} - ${obj[key]}`);
+  console.log(`${key} - ${obj[key]} долларов`);
 }
-  */
+*/
 
 /* Задание 5
 let n = 1000;
@@ -203,12 +198,14 @@ while (n > 50) {
   num++;
 }
 console.log(`Количество циклов: ${num}`);
+
 */
 
-/* Задание 6 */
+/* Задание 6
 let dayFriday = 1;
 let allDay = 31;
 
 for (let i = dayFriday; i <= allDay; i += 7) {
   console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`);
 }
+  */
